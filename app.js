0cf66a2b9c217cfe768b64490b612208f4de504c
klari <claricesaraivaandrade@gmail.com>
2019-04-06 15:39:52 -0300

    qualquer t

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-angular.module("aplicacaoEvento", ['ngMessages','ngRoute'])
+
+angular.module("aplicacaoEvento", ['ngMessages','ngRoute']).config(function($routeProvider) {
+    $routeProvider
+
+        .when('/', {
+            template: "Home",
+        })
+        .when('/eventos', {
+        templateUrl: "componentes/listar/listar.html",
+    })
+        .when('/categoria', {
+            templateUrl: "<categoria></categoria>",
+        })
+        .when('/eventos', {
+            templateUrl: "componentes/listar/listar.html",
+        })
+        .when('/eventos', {
+            templateUrl: "componentes/listar/listar.html",
+        })
+        .when('/eventos', {
+            templateUrl: "componentes/listar/listar.html",
+        })
+        .when('/eventos', {
+            templateUrl: "componentes/listar/listar.html",
+        });
+
+});
